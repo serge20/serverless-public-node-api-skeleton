d15913d1e75d898d063f65606091aa7437cbb38a
Serge Basile <serge@gumgum.com>
2018-05-25 17:47:51 -0700

    Added babel-runtime/body parser

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const serverless = require('serverless-http');
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
+
+app.use(bodyParser.json()); // support json encoded bodies
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
